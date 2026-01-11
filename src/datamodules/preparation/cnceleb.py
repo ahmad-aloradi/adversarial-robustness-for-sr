83e15c01366735720503e70b78af4d2b34333380
Ahmad Aloradi <ahmad.aloradi94@gmail.com>
2026-01-11 19:43:20 +0100

    :ambulance: pass concat mapping to CNCelebProcessor

--- src/datamodules/preparation/cnceleb.py
+++ src/datamodules/preparation/cnceleb.py
@@ -186,6 +186,7 @@ class CNCelebMetadataPreparer(BaseMetadataPreparer):
             segment_overlap=dataset.segment_overlap,
             min_segment_duration=dataset.min_segment_duration,
             vad=dataset.get("vad", None),
+            concat_mapping_file=dataset.get("concat_mapping_file", None),
         )
             
         return processor
